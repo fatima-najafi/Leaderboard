796589c4a812bb04f27de54006cc03bf33fd000a
Fatima Najafi <najafifatima762@gmail.com>
2023-07-26 19:04:07 +0430

    add api for leaderboard project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import './index.css';
+import { refresh, sendData } from './module/data.js';
 
-const btn = document.querySelector('.reload');
-btn.addEventListener('click', () => {
-  console.log(btn);
+document.addEventListener('DOMContentLoaded', () => {
+  const addBtn = document.querySelector('#submit');
+  const refreshBtn = document.querySelector('#refresh');
+
+  // Hundle refresh click event
+
+  refreshBtn.addEventListener('click', () => {
+    refresh();
+  });
+
+  // Calls refresh method when page loads
+
+  document.addEventListener('DOMContentLoaded', refresh);
+
+  // Hundle submit button click event
+  addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const userInput = document.querySelector('#name');
+    const scoreInput = document.querySelector('#score');
+    const user = userInput.value;
+    const score = parseInt(scoreInput.value, 10);
+    sendData(user, score);
+
+    userInput.value = '';
+    scoreInput.value = '';
+  });
 });
